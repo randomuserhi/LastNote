36b5bd36300dffd95763d01a312ee258a942d91d
randomuserhi <40913834+randomuserhi@users.noreply.github.com>
2021-10-04 23:09:49 +0100

    Created a functional window

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-//Main process
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const fs = require("fs");
 
-const {BrowserWindow, app} = require('electron');
-const url = require('url');
+let win;
 
-function boot() 
+async function createWindow()
 {
+	// Create the browser window
 	win = new BrowserWindow({
 		frame: false,
-		
+		show: false,
+		webPreferences: {
+	        nodeIntegration: false, // is default value after Electron v5
+			contextIsolation: true, // protect against prototype pollution
+			enableRemoteModule: false, // turn off remote
+			preload: path.join(__dirname, "preload.js") // use a preload script
+	    }
 	});
-	win.loadURL(url.format({
-		pathname: 'index.html',
-		slashes: true
-	}));
+	win.maximize();
+	win.show();
+
+	win.loadFile(path.join(__dirname, "index.html"));
 }
 
-app.on('ready', boot);
+app.on("ready", createWindow);
+app.on('window-all-closed', function(){
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+});
+
+//Title bar controls
+ipcMain.on('closeWindow', () => {
+    win.close();
+});
+ipcMain.on('maximizeWindow', () => {
+    win.isMaximized() ? win.unmaximize() : win.maximize();
+});
+ipcMain.on('minimizeWindow', () => {
+    win.minimize();
+});
