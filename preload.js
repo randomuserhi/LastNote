c7216b90f08a23ac9b26330d3945e2c7def5e043
randomuserhi <magicalworld.nano@gmail.com>
2022-11-04 02:49:32 +0000

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,8 @@
 const { contextBridge, ipcRenderer } = require("electron"); // Import contextBridge, ipcRenderer from electron module
 
 // Expose methods to the renderer process such that the
-// ipcRenderer can be used without exposing the entire object
+// ipcRenderer can be used without exposing the entire object 
+// (which would be dangerous as the render context can then call ipcRenderer methods)
 // - [contextBridge](https://www.electronjs.org/docs/latest/api/context-bridge)
 // - [ipcRenderer](https://www.electronjs.org/docs/latest/api/ipc-renderer)
 contextBridge.exposeInMainWorld(
