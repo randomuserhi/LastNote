baec01c251772f762994a4548ce2de314febe5b0
randomuserhi <magicalworld.nano@gmail.com>
2022-11-04 02:38:00 +0000

    Update control.js

--- control.js
+++ control.js
@@ -2,11 +2,11 @@
 document.getElementById("close-btn").addEventListener("click", (e) => {
     window.api.closeWindow();
 });
-// Add "click" event to close-btn element which calls the exposed method .api.closeWindow() from preload.js
+// Add "click" event to max-btn element which calls the exposed method .api.closeWindow() from preload.js
 document.getElementById("max-btn").addEventListener("click", (e) => {
     window.api.maximizeWindow();
 });
-// Add "click" event to close-btn element which calls the exposed method .api.closeWindow() from preload.js
+// Add "click" event to min-btn element which calls the exposed method .api.closeWindow() from preload.js
 document.getElementById("min-btn").addEventListener("click", (e) => {
     window.api.minimizeWindow();
 });
