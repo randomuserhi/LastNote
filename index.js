914c264234c4f461e88813fbe773327647c23a6b
randomuserhi <magicalworld.nano@gmail.com>
2022-11-04 02:04:14 +0000

    Added comments

--- index.js
+++ index.js
@@ -1,42 +1,60 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const fs = require("fs");
+const { app, BrowserWindow, ipcMain } = require("electron"); // Import app, BrowserWindow, ipcMain from electron module
+const path = require("path"); // Import path module
+const fs = require("fs"); // Import file system module
 
-let win;
+let win; // Global variable to store main window
 
-async function createWindow()
+/**
+ * Creates the main browser window
+ */
+async function CreateWindow()
 {
 	// Create the browser window
 	win = new BrowserWindow({
-		frame: false,
-		show: false,
+		frame: false, // remove the window frame
+		show: false, // hide the window
 		webPreferences: {
 	        nodeIntegration: false, // is default value after Electron v5
-			contextIsolation: true, // protect against prototype pollution
-			enableRemoteModule: false, // turn off remote
+			contextIsolation: true, // protect against prototype pollution - (https://www.electronjs.org/docs/latest/tutorial/context-isolation)
+			enableRemoteModule: false, // turn off remote - (https://www.npmjs.com/package/@electron/remote) 
+			                           //                 - Note this may have changed for newer electron versions (https://stackoverflow.com/questions/69059668/enableremotemodule-is-missing-from-electron-v14-typescript-type-definitions)
 			preload: path.join(__dirname, "preload.js") // use a preload script
 	    }
 	});
-	win.maximize();
-	win.show();
+	win.maximize(); // maximize the window
+	win.show(); // show the window
 
-	win.loadFile(path.join(__dirname, "index.html"));
+	win.loadFile(path.join(__dirname, "index.html")); // load the main page
 }
 
-app.on("ready", createWindow);
-app.on('window-all-closed', function(){
-    if(process.platform !== 'darwin'){
+/**
+ * This is an example function with an example comment following (https://google.github.io/styleguide/jsguide.html)
+ * In particular, (https://google.github.io/styleguide/jsguide.html#features-functions)
+ * @param {object} param - some parameter that means something
+ * @param {int} otherParam - some other parameter that means something
+ * @return {string} some return value that means something
+ */
+function doSomething(param, otherParam)
+{
+
+}
+
+app.on("ready", CreateWindow); // call createWindow function on "ready" event from app
+app.on("window-all-closed", function(){ // quit app on "window-all-closed" event from app
+    if(process.platform !== 'darwin'){ // If the platform is darwin (https://en.wikipedia.org/wiki/Darwin_(operating_system)) then call app.quit() to finalize
         app.quit();
     }
 });
 
 //Title bar controls
-ipcMain.on('closeWindow', () => {
+//- https://www.electronjs.org/docs/latest/api/ipc-main
+
+ipcMain.on("closeWindow", () => { // when ipcMain recieves "closeWindow" event close the window
     win.close();
 });
-ipcMain.on('maximizeWindow', () => {
+ipcMain.on("maximizeWindow", () => { // when ipcMain recieves "maximizeWindow" event maximize / unmaximize the window
     win.isMaximized() ? win.unmaximize() : win.maximize();
 });
-ipcMain.on('minimizeWindow', () => {
+ipcMain.on("minimizeWindow", () => { // when ipcMain recieves "minimizeWindow" event minimize the window
     win.minimize();
 });
--- preload.js
+++ preload.js
@@ -2,15 +2,17 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
+// - [contextBridge](https://www.electronjs.org/docs/latest/api/context-bridge)
+// - [ipcRenderer](https://www.electronjs.org/docs/latest/api/ipc-renderer)
 contextBridge.exposeInMainWorld(
     "api", {
-        closeWindow: () => {
+        closeWindow: () => { // When window.api.closeWindow() is called, send "closeWindow" event to ipcMain
             ipcRenderer.send('closeWindow');
         },
-        minimizeWindow: () => {
+        minimizeWindow: () => { // When window.api.closeWindow() is called, send "minimizeWindow" event to ipcMain
             ipcRenderer.send('minimizeWindow');
         },
-        maximizeWindow: () => {
+        maximizeWindow: () => { // When window.api.closeWindow() is called, send "maximizeWindow" event to ipcMain
             ipcRenderer.send('maximizeWindow');
         }
     }
